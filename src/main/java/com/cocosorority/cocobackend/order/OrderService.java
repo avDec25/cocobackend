dafc572b1fab97bac3f7fca270dd1cd7fd534e8f
Amar Vashishth <vashishth.amar@gmail.com>
2023-09-22 00:49:46 +0900

    chore: optimised imports

--- src/main/java/com/cocosorority/cocobackend/order/OrderService.java
+++ src/main/java/com/cocosorority/cocobackend/order/OrderService.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import io.micrometer.common.util.StringUtils;
-
 @Service
 public class OrderService {
 
